7a56535c6c22ee492ca6d67b666b4a033adcda5b
hminid <hminid@163.com>
2026-06-03 14:49:23 +0800

    Update diy-part1.sh

--- MT7981-eMMC-USB-hanwckf/diy-part1.sh
+++ MT7981-eMMC-USB-hanwckf/diy-part1.sh
@@ -9,13 +9,3 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone https://github.com/EasyTier/luci-app-easytier package/easytier
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
